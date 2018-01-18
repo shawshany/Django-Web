4ddb44d72f14ed74f6b7c58f770716229436dd5b
Ora Yang <victor.xsyang@gmail.com>
2018-01-18 20:07:52 +0800

    Add files via upload
    Django web develop

---
+++ learning_logs/urls.py
@@ -0,0 +1,12 @@
+"""Defines url patterns for learning_logs."""
+
+from django.urls import path
+
+from . import views
+app_name = 'learning_logs'
+urlpatterns = [
+    # Home page.
+    path('', views.index, name='index'),
+    path('topics',views.topics,name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+]
